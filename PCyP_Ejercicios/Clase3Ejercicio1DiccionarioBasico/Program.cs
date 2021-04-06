78bff34849f54d03f066ffc06aae6bdec2cf2233
AriPaez <paezariel69@gmail.com>
2021-04-06 18:03:43 -0300

    se implementa diccionario

---
+++ PCyP_Ejercicios/Clase3Ejercicio1DiccionarioBasico/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase3Ejercicio1DiccionarioBasico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Diccionario dic = new Diccionario();
+
+            dic.buscarLLave("Juan");
+
+            Console.WriteLine(dic.obtenerLlave("pedro"));
+
+            dic.imprmir();
+
+            dic.alterarValor("Mariana", "58251425");
+
+            Console.ReadLine();
+
+        }
+    }
+}

d358eaf4b84acf2cdceba79b6c4d32e8df5453f6
AriPaez <paezariel69@gmail.com>
2021-04-07 10:36:09 -0300

    se implementa constructor

---
+++ PCyP_Ejercicios/Clase3Ejercicio2ManejoListas/ManejoListas.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase3Ejercicio2ManejoListas
+{
+    class ManejoListas
+    {
+
+        private List<string> colores;
+        private List<string> quitarColores;
+        private static readonly string[] colors = { "MAGENTA", "RED", "WHITE", "BLUE", "CYAN" };
+        private static readonly string[] removeColors = { "RED", "WHITE", "BLUE" };
+
+
+        public ManejoListas()
+        {
+            colores = new List<string>();
+            quitarColores = new List<string>();
+        }
+
+      
+
+
+    }
+}

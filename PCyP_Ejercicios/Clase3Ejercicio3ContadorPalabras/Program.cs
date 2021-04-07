7e2ec57c64e69674df5654dcf137e673434b2d35
AriPaez <paezariel69@gmail.com>
2021-04-07 13:16:22 -0300

    se implementa contadorDePalabras

---
+++ PCyP_Ejercicios/Clase3Ejercicio3ContadorPalabras/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase3Ejercicio3ContadorPalabras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un parrafo");
+
+            ContadorDePalabras cP = new ContadorDePalabras();
+
+            string parrafo = Console.ReadLine();
+
+            
+
+            cP.ingresarParrafo(parrafo);
+            cP.imprimir();
+
+        }
+    }
+}

edbcd371de05956c906f9bccd4658affb33a936c
AriPaez <paezariel69@gmail.com>
2021-04-22 16:12:50 -0300

    Se implementa producto de Matriz

---
+++ PCyP_Ejercicios/Clase5MultiplicacionDeMatrices/Matriz.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase5MultiplicacionDeMatrices
+{
+    class Matriz
+    {
+
+        private int[,] matriz;
+        private int filas, columnas;
+
+        public Matriz(int filas, int columnas)
+        {
+            this.filas = filas;
+            this.columnas = columnas;
+
+            matriz = new int[filas, columnas];
+        }
+
+
+        public void cargarMatriz()
+        {
+            Console.WriteLine("Ingrese valores en la Matriz\n");
+
+            try
+            {
+                for (int i = 0; i < filas; i++)
+                {
+                    for (int j = 0; j < columnas; j++)
+                    {
+                        matriz[i, j] = int.Parse(Console.ReadLine());
+                    }
+
+                }
+
+            }
+            catch (System.FormatException e)
+            {
+                throw;
+
+            }
+
+        }
+
+
+        public void imprimirMatriz()
+        {
+            for (int i = 0; i < filas; i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < columnas; j++)
+                {
+                    Console.Write($"{matriz[i, j]}  ");
+                }
+
+            }
+
+        }
+
+
+        public static void producto(Matriz matrizA, Matriz matrizB)
+        {
+            int suma = 0;
+
+
+            if (matrizA.getColumnas() != matrizB.getFilas())
+            {
+                Console.WriteLine("No se puede realizar la multiplicacion!");
+                return;
+            }
+
+
+
+            Matriz matrizMultiplicacion = new Matriz(matrizA.getFilas(), matrizB.getColumnas());
+
+            for (int i = 0; i < matrizA.getFilas(); i++)
+            {
+
+                for (int j = 0; j < matrizB.getColumnas(); j++)
+                {
+                    suma = 0;
+                    for (int k = 0; k < matrizA.getColumnas(); k++)
+                    {
+                        suma += matrizA.getMatriz()[i, k] * matrizB.getMatriz()[k, j];
+                    }
+
+                    matrizMultiplicacion.getMatriz()[i, j] = suma;
+                }
+            }
+
+            Console.WriteLine("--MatrizA--");
+            matrizA.imprimirMatriz();
+            Console.WriteLine("\n--MatrizB--");
+            matrizB.imprimirMatriz();
+            Console.WriteLine("\n--Producto de MatrizA y MatrizB--");
+            matrizMultiplicacion.imprimirMatriz();
+
+            Console.ReadLine();
+
+        }
+
+
+        public int getFilas() => this.filas;
+        public int getColumnas() => this.columnas;
+        public int[,] getMatriz() => this.matriz;
+
+    }
+}
---
+++ PCyP_Ejercicios/Clase5MultiplicacionDeMatrices/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase5MultiplicacionDeMatrices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int filas = 0;
+            int columnas = 0;
+
+
+
+            try
+            {
+
+                Console.WriteLine("Ingrese fila de MatrizA");
+                filas = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese columna de MatrizA");
+                columnas = int.Parse(Console.ReadLine());
+                Matriz matrizA = new Matriz(filas, columnas);
+
+
+                Console.WriteLine("Ingrese fila de MatrizB");
+                filas = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese columna de MatrizB");
+                columnas = int.Parse(Console.ReadLine());
+                Matriz matrizB = new Matriz(filas, columnas);
+
+                matrizA.cargarMatriz();
+                matrizB.cargarMatriz();
+                Matriz.producto(matrizA, matrizB);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error!Ingrese solo valores enteros.");
+                Console.ReadLine();
+            }
+
+
+
+        }
+    }
+}

2f4fa70207fb4543d29af91872a39e2af57cb9d3
AriPaez <paezariel69@gmail.com>
2021-04-29 18:02:42 -0300

    implementacion de tareas

---
+++ PCyP_Ejercicios/Clase6Abecedario/Alphabet.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Clase6Abecedario
+{
+    class Alphabet
+    {
+
+        public Alphabet() { }
+
+
+        public void alphabetA()
+        {
+            for(int i='A';i<='Z';i++)
+            {
+                Thread.Sleep(500);
+                Console.WriteLine((char)i);
+                
+            }
+        }
+
+        public void alphabetB()
+        {
+            for (int i = 'A'; i <= 'Z'; i++)
+            {
+                Thread.Sleep(500);
+                Console.WriteLine((char)i);
+               
+
+            }
+
+        }
+
+    }
+}
---
+++ PCyP_Ejercicios/Clase6Abecedario/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase6Abecedario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Alphabet abc = new Alphabet();
+
+            Task taskA = Task.Factory.StartNew(abc.alphabetA);
+            
+            Task taskB = Task.Factory.StartNew(abc.alphabetB);
+
+            var tasks = new Task[]{taskA,taskB};
+
+            Task.WaitAll(tasks);
+
+            Console.ReadLine();
+
+        }
+    }
+}
